fc1e39e8f3f2623d65355e06cbced73f0f5e448c
FariaMobeen <113133002+FariaMobeen@users.noreply.github.com>
2023-02-12 16:10:23 -0700

    Update ex1.5.py

--- ex1.5.py
+++ ex1.5.py
@@ -1,4 +1,5 @@
 # Time the original code and your improved version, for all integers between 0 and 35, and plot the results in one graph
+#code from chatgpt
 import time
 import matplotlib.pyplot as plt
 
